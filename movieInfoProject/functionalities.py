81fb507933be315081d8d83be367487877c937c9
Keira Xing <keiraxing@Keiras-MacBook-Pro.local>
2023-03-05 21:08:25 -0500

    R8a fuzzy key word implementation

--- movieInfoProject/functionalities.py
+++ movieInfoProject/functionalities.py
@@ -29,4 +29,26 @@ class Functionalities:
         result.index = np.arange(1, n + 1)
         return result
     
-    
+    def fuzz_search (self, n:str) -> pd.DataFrame:
+        result = self.ctrl.query(f"""
+        WITH midList(mids) as (
+        (SELECT DISTINCT movieID 
+        FROM MOVIE
+        WHERE name LIKE '%{n}%')
+        UNION 
+        (SELECT DISTINCT movieID
+        FROM ACTS natural join ACTOR
+        WHERE name LIKE '%{n}%')
+        UNION 
+        (SELECT DISTINCT movieID
+        FROM DIRECTOR natural join DIRECTS
+        WHERE name LIKE '%{n}%')
+        )
+
+        SELECT *
+        FROM MOVIE as m, midList,ACTOR a, ACTS ar, DIRECTOR d, DIRECTS dr 
+        WHERE ar.actorID  = a.actorID and ar.movieID = m.movieID and 
+        dr.directorID  = d.directorID and dr.movieID = m.movieID and
+        m.movieID = midList.mids;
+                                 """)
+        return result
