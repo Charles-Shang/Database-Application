88ac6794350a580e686fb7911ce1ac24ad31debf
Keira Xing <keiraxing@Keiras-MacBook-Pro.local>
2023-03-05 21:53:37 -0500

    adding group_concate to display actor in one line

--- movieInfoProject/functionalities.py
+++ movieInfoProject/functionalities.py
@@ -45,10 +45,12 @@ class Functionalities:
         WHERE name LIKE '%{n}%')
         )
 
-        SELECT m.name, m.region, m.year, m.category, m.rates, m.summary, a.name, d.name 
+        SELECT m.name as title, m.region, m.year, m.category, m.rates, m.summary, d.name as director_name,
+        GROUP_CONCAT(a.name) as actor_name
         FROM MOVIE as m, midList,ACTOR a, ACTS ar, DIRECTOR d, DIRECTS dr 
         WHERE ar.actorID  = a.actorID and ar.movieID = m.movieID and 
-        dr.directorID  = d.directorID and dr.movieID = m.movieID and
-        m.movieID = midList.mids;
+            dr.directorID  = d.directorID and dr.movieID = m.movieID and
+            m.movieID = midList.mids
+        GROUP BY(m.name);
                                  """)
         return result
