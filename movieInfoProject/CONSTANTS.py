3cf60b3a8231d1821e8394c8a02a55a7a6f2d9a2
vickyyao <vkyaoyikun@gmail.com>
2023-03-06 22:06:24 -0600

    Need to fix this constant

--- movieInfoProject/CONSTANTS.py
+++ movieInfoProject/CONSTANTS.py
@@ -4,7 +4,7 @@ password = 'cs348group11'
 host = 'cs348project.ceggb9lnr3wd.us-west-1.rds.amazonaws.com'
 port = '3306'
 sample_database_name = 'sampledb'
-TABLES = ['ACTOR', 'ACTS', 'DIRECTOR', 'DIRECTS', 'MOVIE', 'RATES', 'USER']
+TABLES = ['ACTOR', 'ACTS', 'DIRECTOR', 'DIRECTS', 'MOVIE', 'RATES', 'USER']  # FIXME
 
 # project
 app_name = "xxx Movie"
