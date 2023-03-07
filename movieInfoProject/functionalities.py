1a5da4ddd8cf95a00fc53e221f07fd2da755204b
Young <you@example.com>
2023-03-06 23:45:51 -0500

    small fix

--- movieInfoProject/functionalities.py
+++ movieInfoProject/functionalities.py
@@ -109,13 +109,13 @@ class Functionalities:
         WITH MOVIE(name, category, rates) as
         (SELECT Movie.name, Movie_category.category, Movie.avg_rate FROM Movie, Movie_category WHERE Movie.id = Movie_category.movie_id),
         temporaryTop3Category(category, averageRating) as
-        (SELECT category, AVG(rates) FROM MOVIE GROUP BY category ORDER BY AVG(rates) desc LIMIT {m})
+        (SELECT category, AVG(rates) FROM MOVIE GROUP BY category ORDER BY AVG(rates) desc LIMIT {n})
         SELECT category, averageRating, name, rates FROM (
         SELECT category, averageRating, name, rates, ROW_NUMBER() OVER (PARTITION BY MOVIE.category ORDER BY MOVIE.rates DESC) AS num
         FROM temporaryTop3Category NATURAL JOIN MOVIE 
         ORDER BY averageRating DESC, rates DESC
         ) AS withNum
-        WHERE withNum.num <= {n};
+        WHERE withNum.num <= {m};
         """)
         result.index = np.arange(1, len(result) + 1)
         return result
