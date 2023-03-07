f801286ae33222d6df30c13637b661d136b06e84
vickyyao <vkyaoyikun@gmail.com>
2023-03-06 21:58:46 -0600

    Fixing R6A for new schema

--- movieInfoProject/functionalities.py
+++ movieInfoProject/functionalities.py
@@ -2,6 +2,7 @@ from CONSTANTS import user, password, host, port, sample_database_name, TABLES
 from mysqlCtrl import MysqlCtrl
 import pandas as pd
 import numpy as np
+from sqlalchemy import text
 
 
 class Functionalities:
@@ -20,10 +21,10 @@ class Functionalities:
         """
 
         result = self.ctrl.query(f"""
-        SELECT name as Title, rates as Rate
-        FROM MOVIE
-        ORDER BY rates DESC
-        LIMIT {n};
+            SELECT name as Title, rates as Rate
+            FROM MOVIE
+            ORDER BY rates DESC
+            LIMIT {n};
         """)
 
         result.index = np.arange(1, len(result) + 1)
@@ -31,31 +32,34 @@ class Functionalities:
 
     def top_actors_with_best_movie(self, n: int = 5) -> pd.DataFrame:
         """
-        top_actors_with_best_movie find the top n actors with best movie.
+        Return the top n actors and their best movie. 
 
         Args:
-            n: The number displayed result, by default n=5
+            n: Number of actors to select. By default, n=5.
 
         Returns: A Table of (actor, movie_name)
         """
-
-        result = self.ctrl.query(f"""
-            SELECT a.name as actor_name,
-                AVG(m.rates) as avg_rating,
-                MAX(m.rates) as best_movie_rating,
-                m.name as best_movie_name
-            FROM ACTOR a
-            JOIN ACTS ac ON a.actorID = ac.actorID
-            JOIN MOVIE m ON ac.movieID = m.movieID
-            GROUP BY a.actorID
-            ORDER BY avg_rating DESC
-            LIMIT {n};
-        """)
-        
+        query = f"""
+            WITH T (actor_name, avg_rating, best_movie_rating, best_movie_name) AS (
+                SELECT C.name as actor_name,
+                    AVG(M.avg_rate) as avg_rating,
+                    MAX(M.avg_rate) as best_movie_rating,
+                    M.name as best_movie_name
+                FROM Actor A
+                JOIN Acts AC ON A.id = AC.actor_id
+                JOIN Movie M ON M.id = AC.movie_id
+                JOIN Celebrity C on A.id = C.id
+                GROUP BY A.id
+                ORDER BY avg_rating DESC
+                LIMIT {n}
+            )
+            SELECT actor_name, best_movie_name FROM T;
+        """
+        result = self.ctrl.query(text(query))
         result.index = np.arange(1, len(result) + 1)
         return result
-    
-    def fuzz_search (self, n:str) -> pd.DataFrame:
+
+    def fuzz_search(self, n: str) -> pd.DataFrame:
         """
        fuzz_search find the movie which contains the keyword in any of
        author name, director name or movie name.
@@ -66,7 +70,7 @@ class Functionalities:
         Returns: A Table of (movie_name, region, year, category, rating,
                              summary, director_name, [actor_name])
         """
-        
+
         result = self.ctrl.query(f"""
         WITH midList(mids) as (
         (SELECT DISTINCT movieID 
@@ -94,7 +98,7 @@ class Functionalities:
         result.index = np.arange(1, len(result) + 1)
         return result
 
-    def find_top_m_movies_for_n_categories (self, n: int = 5, m: int = 3) -> pd.DataFrame:
+    def find_top_m_movies_for_n_categories(self, n: int = 5, m: int = 3) -> pd.DataFrame:
         """
         Top n movie category of average ratings and m top movies in each category
 
@@ -104,7 +108,7 @@ class Functionalities:
         
         Returns: A Table of (category, averageRating, name, rates)
         """
-        
+
         result = self.ctrl.query(f"""
         WITH temporaryTop3Category(category, averageRating) as
         (SELECT category, AVG(rates) FROM MOVIE GROUP BY category ORDER BY AVG(rates) desc LIMIT {n})
