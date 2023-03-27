7259aaa148712b795686fff7a603f4f826d7bb57
Jiaqi Shang <62733535+Charles-Shang@users.noreply.github.com>
2023-03-27 17:35:15 -0400

    Added error raising when using SQL execute
    Signed-off-by: Jiaqi Shang <62733535+Charles-Shang@users.noreply.github.com>

--- implementation/mysqlCtrl.py
+++ implementation/mysqlCtrl.py
@@ -7,11 +7,15 @@ class MysqlCtrl:
     def __init__(self, user, password, host, port, database_name):
         self.engine = create_engine(f'mysql://{user}:{password}@{host}:{port}/{database_name}')
     
-    # def execute(self, sql_stmt):
-    #     with self.engine.connect() as connection:
-    #         connection.execution_options(isolation_level="AUTOCOMMIT")
-    #         connection.execute(sql_stmt)
-    
+    def execute(self, sql_stmt) -> bool:
+        try:
+            with self.engine.connect() as connection:
+                connection.execution_options(isolation_level="AUTOCOMMIT")
+                connection.execute(sql_stmt)
+            return True
+        except Exception as e:
+            return False
+
     def query(self, sql_stmt):
         # print(sql_stmt)
         with self.engine.connect() as connection:
@@ -22,5 +26,6 @@ class MysqlCtrl:
 if __name__ == "__main__":
     from CONSTANTS import user, password, host, port, sample_database_name
     sqlctrl = MysqlCtrl(user, password, host, port, sample_database_name)
-    result = sqlctrl.query("SELECT * FROM Movie Limit 5;")
-    print(result)
+    # result = sqlctrl.execute("DELETE FROM Permission where id=11")
+    # result = sqlctrl.execute("INSERT INTO Permission VALUES(11, 'permission11')")
+    # print(result)
