9644a5d6eb36573dc3973caf5ecdc2be07c252c6
Young <you@example.com>
2023-03-27 12:34:34 -0400

    R7c & R11-2 & R11-3

--- movieInfoProject/functionalities.py
+++ movieInfoProject/functionalities.py
@@ -119,3 +119,69 @@ class Functionalities:
         """)
         result.index = np.arange(1, len(result) + 1)
         return result
+    
+    def director_relative_rating (self) -> pd.DataFrame:
+        """
+        The relative rating for all directors (based on the average rating of the average rating for all the movies they directed)
+
+        Args:
+            none
+        
+        Returns: A Table of (director_id, first_name, last_name, relative_rating)
+        """
+        
+        result = self.ctrl.query(f"""
+        WITH AverageRating(movie_id, average_rating) AS
+        (
+        SELECT RateBy.movie_id, AVG(Rating.value)
+        FROM RateBy, Rating
+        WHERE RateBy.movie_id = Rating.movie_id AND RateBy.user_id = Rating .user_id AND Rating.value IS NOT NULL
+        GROUP BY Rating.movie_id
+        )
+        SELECT C.id AS director_id, C.first_name AS first_name, C.last_name AS last_name, AVG(A.average_rating) AS relative_rating
+        FROM Celebrity AS C, Movie as M, AverageRating as A
+        WHERE C.id = M.director_id  AND A.movie_id = M.id
+        GROUP BY C.id 
+        ORDER BY relative_rating DESC
+        """)
+        result.index = np.arange(1, len(result) + 1)
+        return result
+    
+    def number_of_movies_by_good_directors_per_year (self, n : float = 0.1) -> pd.DataFrame:
+        """
+        The number of movies released directed by directors with relative rating of top (n*100)% every year
+
+        Args:
+            n: The percentage of the directors (in decimal) that counts into the number of movies per year
+        
+        Returns: A Table of (year, num)
+        """
+        
+        result = self.ctrl.query(f"""
+        WITH AverageRating(movie_id, average_rating) AS
+        (
+        SELECT RateBy.movie_id, AVG(Rating.value)
+        FROM RateBy, Rating
+        WHERE RateBy.movie_id = Rating.movie_id AND RateBy.user_id = Rating .user_id AND Rating.value IS NOT NULL
+        GROUP BY Rating.movie_id
+        ),
+        DirectorRating(director_id, director_first_name, director_last_name, relative_rating, relative_rank) AS 
+        (
+        SELECT C.id, C.first_name, C.last_name, AVG(A.average_rating) as relative_rating, RANK() OVER (ORDER BY (AVG(A.average_rating)) DESC)
+        FROM Celebrity AS C, Movie as M, AverageRating as A
+        WHERE C.id = M.director_id  AND A.movie_id = M.id
+        GROUP BY C.id 
+        ORDER BY relative_rating DESC
+        )
+        SELECT year, COUNT(*) AS num
+        FROM Movie
+        WHERE director_id IN
+        (
+        SELECT director_id FROM DirectorRating WHERE relative_rank <= (SELECT COUNT(*) FROM DirectorRating)* {n}
+        )
+        GROUP BY year
+        ORDER BY year
+        """)
+        result.index = np.arange(1, len(result) + 1)
+        return result
+    
